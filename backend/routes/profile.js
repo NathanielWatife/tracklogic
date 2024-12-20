50d6a6cbf3368e42dcbb4e650adaefb5cb9c57ca
NathanielWatife <awosanminathaniel0@gmail.com>
2024-12-20 22:12:26 +0100

    Worked on user profile update

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // private access only
 router.get('/', authMiddleware, async (req, res) => {
     try {
-        const user = await Uer.findById(req.user.id).select('-password');
+        const user = await User.findById(req.user.id).select('-password');
         if (!user) return res.status(404).json({ message: 'User not found' });
         res.json(user);
     } catch (err) {

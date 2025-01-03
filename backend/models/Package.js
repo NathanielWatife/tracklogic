fe829db1dc0ae005b92f4d3406ca25ff3fc42e34
NathanielWatife <awosanminathaniel0@gmail.com>
2025-01-03 16:23:28 +0100

    Added payment features

--- backend/models/Package.js
+++ backend/models/Package.js
@@ -1,20 +1,39 @@
 const mongoose = require('mongoose');
 
 const PackageSchema = new mongoose.Schema({
-    packageId: { type: String,  unique: true, required: true },
-    userId: {  type: mongoose.Schema.Types.ObjectId, ref: 'User',  required: true },
-    weight: { type: Number,  required: true  },
-    quantity: { type: Number, required: true  },
+    packageId: { type: String, unique: true, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    weight: { type: Number, required: true },
+    quantity: { type: Number, required: true },
     dimensions: { type: String },
-    transitType: { type: String, enum : ['Bike', 'Car', 'Van', 'MiniVan'], required: true },
-    deliveryType: { type: String, enum : ['PickupStation', 'DoorDelivery'], required: true },
-    deliveryAddress: { street: String, city: String, state: String, zipCode: String, landmark: String },
+    transitType: { type: String, enum: ['Bike', 'Car', 'Van', 'MiniVan'], required: true },
+    deliveryType: { type: String, enum: ['PickupStation', 'DoorDelivery'], required: true },
+    deliveryAddress: {
+        street: String,
+        city: String,
+        state: String,
+        zipCode: String,
+        landmark: String,
+    },
     price: { type: Number, required: true },
-    status: { type: String, enum: ['Pending', 'In-Transit', 'Delivered'], default: 'Pending' },
-    currentLocation: { latitude: { type: Number, required: false }, longitude: { type: Number, reuired: false }, timstamp: { type: Date, default: Date.now }, },
+    status: { type: String, enum: ['Pending', 'In Transit', 'Delivered'], default: 'Pending' },
+    currentLocation: {
+        latitude: { type: Number, required: false },
+        longitude: { type: Number, required: false },
+        timestamp: { type: Date, default: Date.now },
+    },
     driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver' },
     vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' },
-    createdAt: {type: Date, default: Date.now}
+    payment: {
+        status: { type: String, enum: ['Pending', 'Paid', 'Failed'], default: 'Pending' },
+        method: { type: String, enum: ['BankTransfer', 'USSD', 'Paystack'], required: false },
+        transactionId: { type: String, required: false },
+        amount: { type: Number, required: false },
+        reference: { type: String, required: false },
+        currency: { type: String, default: 'NGN' }, // Default to Naira
+        paymentDate: { type: Date, required: false },
+    },
+    createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Package', PackageSchema);
